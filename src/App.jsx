d730859466a53177f219ec741ec485242adc7bc9
Adelia <adel.lia.06a@gmail.com>
2026-04-14 16:54:21 +0600

    hero section 70% done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import Header from "./components/header/Header"
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
 
+const Home = () => <div>
+  <Hero />
+</div>;
+const About = () => <div>Страница О нас</div>;
+const Contact = () => <div >Наши контакты</div>;
+const Blog = () => <div >Наш блог</div>;
 
 const App = () => {
   return (
     <div>
-        <Header /> 
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
